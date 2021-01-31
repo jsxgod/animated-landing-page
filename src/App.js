c4047a88240ab2ff69117630ebac2e3f4345c6aa
jsxgod <smykoo7@gmail.com>
2021-01-31 19:17:26 +0100

    Calculate viewport height using window.innerHeight to include the mobile browser elements.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Banner, Header } from './components/';
 
 import './styles/App.scss';
 
 const App = () => {
+
+    useEffect(() => {
+        let vh = window.innerHeight * .01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }, []);
+
     return (
         <div className="App">
             <Banner />
