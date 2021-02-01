3d1940c578d58ea9a399b3fddce6246ffb934b20
jsxgod <smykoo7@gmail.com>
2021-02-01 16:45:03 +0100

    Clean up imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 export { default as About } from './About/About';
 export { default as Approach } from './Approach/Approach';
 export { default as Home } from './Home/Home';
-export { default as Product } from './Product/Product';
+export { default as Products } from './Products/Products';
+export { default as Product } from './Products/Product/Product';
 export { default as Services } from './Services/Services';
