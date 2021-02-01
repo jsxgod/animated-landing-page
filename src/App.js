6413e6f749ea8b1319e3debdae3413d4e0c14164
jsxgod <smykoo7@gmail.com>
2021-02-01 16:44:52 +0100

    Add route to products page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import gsap from 'gsap';
 import { Route } from 'react-router-dom';
 import { Header } from './components/';
-import { About, Approach, Home, Product, Services } from './pages';
+import { About, Approach, Home, Product, Products, Services } from './pages';
 
 import './styles/App.scss';
 
@@ -11,6 +11,7 @@ const exact_routes = [
     { path: "/about-us", name: "About Us", Component: About},
     { path: "/approach", name: "Approach", Component: Approach},
     { path: "/services", name: "Services", Component: Services},
+    { path: "/products", name: "Products", Component: Products}
 ];
 
 const App = () => {
