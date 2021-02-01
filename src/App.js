79a4de8b5d05d398510a6ca1c89b4b161e800723
jsxgod <smykoo7@gmail.com>
2021-02-01 15:07:25 +0100

    Change project structure. Add pages directory. Move code to appropriate component.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,27 @@
 import React, { useEffect } from 'react'
 import gsap from 'gsap';
-import { Banner, Header, Overlay, Products } from './components/';
+import { Header } from './components/';
+import { Home } from './pages';
 
 import './styles/App.scss';
 
 const App = () => {
 
     useEffect(() => {
-        let vh = window.innerHeight * .01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-        gsap.to('body', {
+        gsap.to("body", {
             duration: 0,
-            css: {visibility: "visible"}
+            css: { visibility: "visible"}
         });
-
-        const tl = gsap.timeline();
-        tl.from(".line div", {
-            duration: 1.8,
-            y: 200,
-            ease: "power4.out",
-            skewY: 7,
-            delay: 1,
-            stagger: {
-                amount: 0.3
-            }
-        })
-        .to('.box-top', {
-            duration: 1.6,
-            height: 0,
-            ease: 'expo.inOut',
-            stagger: 0.4
-        })
-        .to('.box-bottom', {
-            duration: 1.6,
-            width: 0,
-            ease: 'expo.inOut',
-            delay: -0.8,
-            stagger: {
-                amount: 0.4
-            }
-        })
-        .to('.overlay', {
-            duration: 0,
-            css: { 
-                display: "none" 
-            }
-        })
-        .from('.product-image img', {
-            duration: 1.6,
-            scale: 1.4,
-            ease: "expo.inOut",
-            delay: -2,
-            stagger: {
-                amount: 0.4
-            }
-        })
-    }, []);
+    }, [])
 
     return (
-        <div className="App">
-            <Overlay />
+        <>
             <Header />
-            <Banner />
-            <Products />
-        </div>
+            <Home />
+        </>
     )
 }
 
