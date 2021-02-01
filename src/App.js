df29ffa9d3b1bde0b34b6d1c32ea01b22b0a4f44
jsxgod <smykoo7@gmail.com>
2021-02-01 15:42:01 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import gsap from 'gsap';
+import { Route } from 'react-router-dom';
 import { Header } from './components/';
-import { Home } from './pages';
+import { About, Approach, Home, Product, Services } from './pages';
 
 import './styles/App.scss';
+
+const exact_routes = [
+    { path: "/", name: "Home", Component: Home},
+    { path: "/about-us", name: "About Us", Component: About},
+    { path: "/approach", name: "Approach", Component: Approach},
+    { path: "/services", name: "Services", Component: Services},
+];
 
 const App = () => {
 
@@ -20,7 +28,15 @@ const App = () => {
     return (
         <>
             <Header />
-            <Home />
+            <div className="App">
+                {exact_routes.map(({ path, Component }) => (
+                    <Route key={path} exact path={path}>
+                        <Component />
+                    </Route>
+                ))}
+
+                <Route path="/product" component={ Product } />
+            </div>
         </>
     )
 }
