108b99fb6726ff1c9787bf8442f2dcb3c37d95fa
jsxgod <smykoo7@gmail.com>
2021-02-01 03:41:40 +0100

    Animate the overlay

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import gsap from 'gsap';
 import { Banner, Header, Overlay, Products } from './components/';
 
 import './styles/App.scss';
@@ -8,6 +9,53 @@ const App = () => {
     useEffect(() => {
         let vh = window.innerHeight * .01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+        gsap.to('body', {
+            duration: 0,
+            css: {visibility: "visible"}
+        });
+
+        const tl = gsap.timeline();
+        tl.from(".line div", {
+            duration: 1.8,
+            y: 200,
+            ease: "power4.out",
+            skewY: 7,
+            delay: 1,
+            stagger: {
+                amount: 0.3
+            }
+        })
+        .to('.box-top', {
+            duration: 1.6,
+            height: 0,
+            ease: 'expo.inOut',
+            stagger: 0.4
+        })
+        .to('.box-bottom', {
+            duration: 1.6,
+            width: 0,
+            ease: 'expo.inOut',
+            delay: -0.8,
+            stagger: {
+                amount: 0.4
+            }
+        })
+        .to('.overlay', {
+            duration: 0,
+            css: { 
+                display: "none" 
+            }
+        })
+        .from('.product-image img', {
+            duration: 1.6,
+            scale: 1.4,
+            ease: "expo.inOut",
+            delay: -2,
+            stagger: {
+                amount: 0.4
+            }
+        })
     }, []);
 
     return (
