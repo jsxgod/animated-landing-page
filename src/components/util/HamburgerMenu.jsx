62732c56e1934c1ec9ac334d628cfa533c7289a0
jsxgod <smykoo7@gmail.com>
2021-02-03 13:23:21 +0100

    Prepare the HamburgerMenu component for animation. Add onClick event handlers.

--- src/components/util/HamburgerMenu.jsx
+++ src/components/util/HamburgerMenu.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { ReactComponent as UpArrow } from '../../assets/arrow-up-circle.svg';
 
 const HamburgerMenu = () => {
+    const [menuOpened, setMenuOpened] = useState(false);
+
+    useEffect(() => {
+        menuOpened ? 
+            // run open menu animation
+            console.log('open')
+        :
+            // run close menu animation
+            console.log('close')
+
+    }, [menuOpened]);
+
     return (
-        <div className="nav">
-            <span></span>
-            <span></span>
-            <span></span>
+        <div className="nav-toggle">
+            <div onClick={() => setMenuOpened(true)} className="hamburger-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div onClick={() => setMenuOpened(false)} className="hamburger-menu-close">
+                <UpArrow />
+            </div>
         </div>
     )
 }
